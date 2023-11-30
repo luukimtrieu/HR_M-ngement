c74150f3660a25db6388d5b06007e90ecb49376a
tuankiet <89694514+MotChut@users.noreply.github.com>
2023-11-30 21:36:37 +0700

    [Add] Add Category + View Category

--- HR_Management/src/App.jsx
+++ HR_Management/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './Components/Home'
 import Employee from './Components/Employee'
 import Category from './Components/Category'
 import Profile from './Components/Profile'
+import AddCategory from './Components/AddCategory'
 
 function App() {
 
@@ -17,8 +18,9 @@ function App() {
       <Route path='/dashboard' element={<Dashboard />}>
         <Route path='' element={<Home />}></Route>
         <Route path='/dashboard/employee' element={<Employee />}></Route>
-        <Route path='/dashboard//category' element={<Category />}></Route>
-        <Route path='/dashboard//profile' element={<Profile />}></Route>
+        <Route path='/dashboard/category' element={<Category />}></Route>
+        <Route path='/dashboard/profile' element={<Profile />}></Route>
+        <Route path='/dashboard/add_category' element={<AddCategory />}></Route>
       </Route>
       
     </Routes>
--- Server/Routes/AdminRoute.js
+++ Server/Routes/AdminRoute.js
@@ -19,8 +19,23 @@ router.post('/adminlogin', (req, res) => {
         else {
             return res.json({ loginStatus: false, Error: "Wrong email or password!" });
         }
-        
     });
 });
+
+router.post('/add_category', (req, res) => {
+    const sql = "INSERT INTO category (`name`) VALUES (?)"
+    con.query(sql, [req.body.category], (err, result) => {
+        if(err) return res.json({Status: false, Error: "Query Error"})
+        return res.json({Status: true})
+    })
+})
+
+router.get('/category', (req, res) => {
+    const sql = "SELECT * FROM category";
+    con.query(sql, (err, result) => {
+        if(err) return res.json({Status: false, Error: "Query Error"})
+        return res.json({Status: true, Result: result})
+    })
+})
 
 export { router as adminRouter }
