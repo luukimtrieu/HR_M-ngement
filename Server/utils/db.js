cacdce1cb7543414e0bed58d43ffae0b33444442
Trieu Luu <trieukim1410@gmail.com>
2023-12-12 08:10:07 +0700

    employee

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -4,7 +4,7 @@ const con = mysql .createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "hrm"
+    database: "employeems"
 });
 
 con.connect(function(err){
